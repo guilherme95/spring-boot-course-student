5ef754ea7c0b2bd9cf1f17ce4c7b4aa49ba7961c
guilherme95 <guilhermemello62@gmail.com>
2022-03-22 21:22:14 -0300

    spring skeleton with student example

---
+++ src/main/java/br/com/guilherme/student/StudentConfig.java
@@ -0,0 +1,31 @@
+package br.com.guilherme.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student guilherme = new Student(
+                    "Guilherme",
+                    "guilhermemello62@gmail.com",
+                    LocalDate.of(1995, Month.NOVEMBER, 25)
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2005, Month.MARCH, 12)
+            );
+            studentRepository.saveAll(List.of(guilherme, alex));
+        };
+    }
+
+}
---
+++ src/main/java/br/com/guilherme/student/StudentRepository.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
+
+}
